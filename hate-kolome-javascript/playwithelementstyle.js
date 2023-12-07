255303bd1c2f3dcb384d58fc3b140509eb7f448a
ashrafbd1496 <asraf1496@gmail.com>
2023-12-08 00:06:59 +0600

    removed element using different way

--- hate-kolome-javascript/playwithelementstyle.js
+++ hate-kolome-javascript/playwithelementstyle.js
@@ -22,3 +22,21 @@ box1 = document.getElementById("box1");
 box2 = document.getElementById("box2");
 
 box1.appendChild(box2);
+
+//change position of box
+box3 = document.getElementById("box3");
+box3.insertAdjacentElement("beforebegin", box4);
+
+//remove element using child node
+
+console.dir(box4.childNodes);
+box4.childNodes[3].remove();
+
+//remove using child
+var box3 = document.getElementById("box3");
+var box3p = document.querySelector("#box3>p");
+
+box3.removeChild(box3p);
+
+//direct remove
+//box2.remove();
